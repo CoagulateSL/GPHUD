57ba8a97bdbbd58d0bf5cd64edd42b6899847fd0
Iain Price (LUNA) <git@predestined.net>
2018-12-06 02:27:43 +0000

    different Url ;P

--- src/net/coagulate/GPHUD/Modules/Instance/HelpPage.java
+++ src/net/coagulate/GPHUD/Modules/Instance/HelpPage.java
@@ -1,11 +1,11 @@
 package net.coagulate.GPHUD.Modules.Instance;
 
+import net.coagulate.Core.Tools.SystemException;
+import net.coagulate.Core.Tools.UserException;
 import net.coagulate.GPHUD.Interfaces.User.Form;
 import net.coagulate.GPHUD.Modules.URL.URLs;
 import net.coagulate.GPHUD.SafeMap;
 import net.coagulate.GPHUD.State;
-import net.coagulate.Core.Tools.SystemException;
-import net.coagulate.Core.Tools.UserException;
 
 
 /**  Because we need an index for the User Interface.
@@ -13,7 +13,7 @@ import net.coagulate.Core.Tools.UserException;
  * @author Iain Price <gphud@predestined.net>
  */
 public abstract class HelpPage {
-    @URLs(url = "/Docs")
+    @URLs(url = "/Help")
     public static void index(State st,SafeMap values) throws UserException, SystemException {
         Form f=st.form;
         f.add("<iframe width=100% height=100% frameborder=0 src=\"https://sl.coagulate.net/Docs/GPHUD/index.php/Main_Page.html\"></iframe>");
