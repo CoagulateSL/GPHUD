e11e1d378b6b060582cf6ca631d4caaae8aa9f02
Iain Price (LUNA) <git@predestined.net>
2018-12-06 02:31:22 +0000

    scale?

--- src/net/coagulate/GPHUD/Modules/Instance/HelpPage.java
+++ src/net/coagulate/GPHUD/Modules/Instance/HelpPage.java
@@ -16,6 +16,6 @@ public abstract class HelpPage {
     @URLs(url = "/help")
     public static void index(State st,SafeMap values) throws UserException, SystemException {
         Form f=st.form;
-        f.add("<iframe width=100% height=100% frameborder=0 src=\"https://sl.coagulate.net/Docs/GPHUD/index.php/Main_Page.html\"></iframe>");
+        f.add("<table width=100% height=100%><tr width=100% height=100%><td width=100% height=100% valign=top><iframe width=100% height=100% frameborder=0 src=\"https://sl.coagulate.net/Docs/GPHUD/index.php/Main_Page.html\"></iframe></td></tr></table>");
     }
 }
