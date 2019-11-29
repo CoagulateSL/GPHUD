7697f1d50daf6f3c37f5c2facbca3ef62c3cdc5c
Iain Price (LUNA) <git@predestined.net>
2019-11-29 02:17:48 +0000

    Revert "Use <> inference"
    This reverts commit 427d8d5f30e1d71ec462aa2dbe821cfdd69642a9.

--- src/net/coagulate/GPHUD/Modules/Introspection/URLHandlers.java
+++ src/net/coagulate/GPHUD/Modules/Introspection/URLHandlers.java
@@ -31,7 +31,7 @@ public abstract class URLHandlers {
 		Table t = new Table();
 		f.add(t);
 		t.add(new HeaderRow().add("URL").add("Method"));
-		Map<String, Row> output = new TreeMap<>();
+		Map<String, Row> output = new TreeMap<String, Row>();
 		for (Module module : Modules.getModules()) {
 			for (URL url : module.getAllContents(st)) {
 				Row writeup = new Row();
