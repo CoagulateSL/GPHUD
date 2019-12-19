c5665c8a106bf5c2abb29dc54fe8a9f84179363d
Iain Price (LUNA) <git@predestined.net>
2019-12-19 08:35:11 +0000

    Bump v3.11.1

--- src/net/coagulate/GPHUD/GPHUD.java
+++ src/net/coagulate/GPHUD/GPHUD.java
@@ -32,7 +32,7 @@ import static java.util.logging.Level.SEVERE;
  * @author Iain Price <gphud@predestined.net>
  */
 public class GPHUD {
-	public static final String VERSION="v3.11.0";
+	public static final String VERSION="v3.11.1";
 	public static final String VERSION_DATE="Dec 18th 2019";
 	// config KV store
 	private static final Map<String,String> CONFIG=new TreeMap<>();
