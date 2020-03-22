0f901c0b7870616ba4c367793f2bcf6b21c3a02f
Iain Price (LUNA) <git@predestined.net>
2020-03-22 02:30:54 +0000

    Add functions gsGetGroupsByType and gsGetOpenGroupsByType

--- src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Groups.java
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Groups.java
@@ -2,6 +2,7 @@ package net.coagulate.GPHUD.Modules.Scripting.Language.Functions;
 
 import net.coagulate.GPHUD.Data.CharacterGroup;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCCharacter;
+import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCList;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCString;
 import net.coagulate.GPHUD.Modules.Scripting.Language.GSVM;
 import net.coagulate.GPHUD.State;
@@ -22,4 +23,31 @@ public class Groups {
 		if (group!=null) { name=group.getName(); }
 		return new BCString(null,name);
 	}
+
+	@Nonnull
+	@GSFunctions.GSFunction(description="Gets a List of group names for a given group type", returns="A List of Strings containing all groups of that type", parameters=
+			"String - type of group to list", notes="", privileged=false)
+	public static BCList gsGetGroupsByType(final State st,
+	                                       final GSVM vm,
+	                                       @Nonnull final BCString grouptype) {
+		BCList list=new BCList(null);
+		for (CharacterGroup cg: st.getInstance().getGroupsForKeyword(grouptype.toString())) {
+			list.append(new BCString(null,cg.getName()));
+		}
+		return list;
+	}
+
+	@Nonnull
+	@GSFunctions.GSFunction(description="Gets a List of open group names for a given group type", returns="A List of Strings containing all open groups of that type",
+	                        parameters="String - type of group to list", notes="", privileged=false)
+	public static BCList gsGetOpenGroupsByType(final State st,
+	                                           final GSVM vm,
+	                                           @Nonnull final BCString grouptype) {
+		BCList list=new BCList(null);
+		for (CharacterGroup cg: st.getInstance().getGroupsForKeyword(grouptype.toString())) {
+			if (cg.isOpen()) { list.append(new BCString(null,cg.getName())); }
+		}
+		return list;
+	}
+
 }
