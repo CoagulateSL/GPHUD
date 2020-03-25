1442da0aa9dd7d4189bcae23a5e212fe79ecf0d0
Iain Price (LUNA) <git@predestined.net>
2020-03-25 01:58:36 +0000

    Admin permissions to apply/remove effects

--- src/net/coagulate/GPHUD/Modules/Effects/package-info.java
+++ src/net/coagulate/GPHUD/Modules/Effects/package-info.java
@@ -3,6 +3,8 @@
 @Permission.Permissions(name="Edit", description="Permission to edit effects", power=Permission.POWER.LOW)
 @Permission.Permissions(name="Create", description="Permission to create effects", power=Permission.POWER.LOW)
 @Permission.Permissions(name="Delete", description="Permission to delete effects", power=Permission.POWER.MEDIUM)
+@Permission.Permissions(name="Apply", description="Permission to administratively (as an avatar) apply effects to a character",power=Permission.POWER.LOW)
+@Permission.Permissions(name="Remove", description="Permission to administratively (as an avatar) remove an effect from a character",power=Permission.POWER.LOW)
 @KV.KVS(name="ApplyMessage", description="Message to send to the character when they gain this effect", defaultvalue="", editpermission="Effects.Edit", type=KV.KVTYPE.TEXT, template=true, scope=KV.KVSCOPE.EFFECT, hierarchy=KV.KVHIERARCHY.NONE)
 @KV.KVS(name="RemoveMessage", description="Message to send to the character when they lose this effect", defaultvalue="", editpermission="Effects.Edit", type=KV.KVTYPE.TEXT, template=true, scope=KV.KVSCOPE.EFFECT, hierarchy=KV.KVHIERARCHY.NONE)
 package net.coagulate.GPHUD.Modules.Effects;
