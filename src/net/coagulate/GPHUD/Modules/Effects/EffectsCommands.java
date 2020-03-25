16e4c4130a71553a1060a73f1a372f755d0347da
Iain Price (LUNA) <git@predestined.net>
2020-03-25 22:15:08 +0000

    Command effects.apply

--- src/net/coagulate/GPHUD/Modules/Effects/EffectsCommands.java
+++ src/net/coagulate/GPHUD/Modules/Effects/EffectsCommands.java
@@ -1,5 +1,6 @@
 package net.coagulate.GPHUD.Modules.Effects;
 
+import net.coagulate.GPHUD.Data.Char;
 import net.coagulate.GPHUD.Data.Effect;
 import net.coagulate.GPHUD.Interfaces.Responses.OKResponse;
 import net.coagulate.GPHUD.Interfaces.Responses.Response;
@@ -13,11 +14,17 @@ public class EffectsCommands {
 
 	@Command.Commands(description="Applies an effect to a character",requiresPermission="Effects.Apply",context=Command.Context.AVATAR)
 	public static Response apply(@Nonnull final State st,
+	                             @Argument.Arguments(description="Character to apply effect to",type=Argument.ArgumentType.CHARACTER)
+	                             @Nonnull final Char target,
 	                             @Argument.Arguments(description="Name of effect to apply",type=Argument.ArgumentType.EFFECT)
 	                             @Nonnull final Effect effect,
 	                             @Argument.Arguments(description="Duration for effect (in seconds)",type=Argument.ArgumentType.INTEGER)
 	                             final int seconds) {
-		return new OKResponse("No-op");
+		if (effect.apply(st,true,target,seconds)) {
+			return new OKResponse("Effect applied ");
+		} else {
+			return new OKResponse("Effect already exists on target for a longer duration");
+		}
 	}
 
 }
