93214da74060a53595bc01d2c67e3947109ac7b6
Iain Price (LUNA) <git@predestined.net>
2020-04-05 15:53:05 +0100

    Added avatarLookup command

--- src/net/coagulate/GPHUD/Modules/External/BasicCommands.java
+++ src/net/coagulate/GPHUD/Modules/External/BasicCommands.java
@@ -1,14 +1,19 @@
 package net.coagulate.GPHUD.Modules.External;
 
+import net.coagulate.GPHUD.Data.Char;
+import net.coagulate.GPHUD.Data.Zone;
 import net.coagulate.GPHUD.GPHUD;
 import net.coagulate.GPHUD.Interface;
 import net.coagulate.GPHUD.Interfaces.Responses.ErrorResponse;
 import net.coagulate.GPHUD.Interfaces.Responses.JSONResponse;
 import net.coagulate.GPHUD.Interfaces.Responses.OKResponse;
 import net.coagulate.GPHUD.Interfaces.Responses.Response;
+import net.coagulate.GPHUD.Modules.Argument.ArgumentType;
+import net.coagulate.GPHUD.Modules.Argument.Arguments;
 import net.coagulate.GPHUD.Modules.Command.Commands;
 import net.coagulate.GPHUD.Modules.Command.Context;
 import net.coagulate.GPHUD.State;
+import net.coagulate.SL.Data.User;
 import org.json.JSONObject;
 
 import javax.annotation.Nonnull;
@@ -52,5 +57,33 @@ public class BasicCommands {
 			return new ErrorResponse("You have no developer key, please contact Iain Maltz if you believe this to be in error");
 		}
 		return new OKResponse("Your developer key is: "+st.getAvatar().getDeveloperKey());
+	}
+
+	@Commands(description="Look up information about the avatar",
+	          permitScripting=false,
+	          permitObject=false,
+	          permitJSON=false,
+	          permitUserWeb=false,
+	          permitConsole=false,
+	          context=Context.AVATAR)
+
+	@Nonnull
+	public static Response lookupAvatar(@Nonnull final State state,
+	                                    @Arguments(description="Avatar to lookup",
+	                                               type=ArgumentType.AVATAR) @Nonnull final User user) {
+		JSONObject json=new JSONObject();
+		json.put("avatarid",user.getId());
+		json.put("avatarname",user.getUsername());
+		Char character=Char.getActive(user,state.getInstance());
+		if (character!=null) {
+			json.put("playingcharacterid",character.getId());
+			json.put("playingcharactername",character.getName());
+			json.put("playingcharacterregion",character.getRegion().getName());
+			Zone zone=character.getZone();
+			if (zone!=null) {
+				json.put("playingcharacterzone",zone.getName());
+			}
+		}
+		return new JSONResponse(json);
 	}
 }
