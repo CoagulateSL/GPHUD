deafedc515198540dc6c0c4045808b36ba8f1525
Iain Price (LUNA) <git@predestined.net>
2020-04-19 23:25:02 +0100

    WIP

---
+++ src/net/coagulate/GPHUD/Modules/UIX/package-info.java
@@ -0,0 +1,8 @@
+@ModuleDefinition(description="Driver module for the Extended User Interface components in the HUD",
+                  canDisable=false)
+
+package net.coagulate.GPHUD.Modules.UIX;
+
+import net.coagulate.GPHUD.Modules.Module.ModuleDefinition;
+
+
