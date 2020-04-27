3f2e207cc5a3e60d8588640469432572a69fdb83
Iain Price (LUNA) <git@predestined.net>
2020-04-27 01:36:35 +0100

    add serial version UID for serialization interface

--- src/net/coagulate/GPHUD/Interfaces/External/ExternalInterfaceObjectAccessDeniedException.java
+++ src/net/coagulate/GPHUD/Interfaces/External/ExternalInterfaceObjectAccessDeniedException.java
@@ -3,12 +3,13 @@ package net.coagulate.GPHUD.Interfaces.External;
 import net.coagulate.Core.Exceptions.User.UserAccessDeniedException;
 
 public class ExternalInterfaceObjectAccessDeniedException extends UserAccessDeniedException {
+	private static final long serialVersionUID=1L;
+
 	public ExternalInterfaceObjectAccessDeniedException(String reason) {
 		super(reason);
 	}
 
-	public ExternalInterfaceObjectAccessDeniedException(String reason,
-	                                                    Throwable cause) {
+	public ExternalInterfaceObjectAccessDeniedException(String reason,Throwable cause) {
 		super(reason,cause);
 	}
 }
