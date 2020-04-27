15f2d4e4f267bfa6f4d5893f1f59451df52f103e
Iain Price (LUNA) <git@predestined.net>
2020-04-27 01:28:48 +0100

    Add permission External.ConnectObjects

--- src/net/coagulate/GPHUD/Modules/External/package-info.java
+++ src/net/coagulate/GPHUD/Modules/External/package-info.java
@@ -1,9 +1,8 @@
 @ModuleDefinition(description="Allows configuration of the External Access API.  See documentation.",
-                  defaultDisable=true) @Permissions(description="Allows authorisation of a developer to the instance",
-                                                    power=POWER.HIGH,
-                                                    name="Authorise") @Permissions(description="Allows deauthorisation of a developer to the instance",
-                                                                                   power=POWER.MEDIUM,
-                                                                                   name="DeAuthorise")
+                  defaultDisable=true) @Permissions(description="Allows authorisation of a developer to the instance", power=POWER.HIGH, name="Authorise") @Permissions(
+		description="Allows deauthorisation of a developer to the instance",
+		power=POWER.MEDIUM,
+		name="DeAuthorise") @Permissions(description="Allows the user to connect external objects to this instance", name="ConnectObjects", power=POWER.MEDIUM)
 package net.coagulate.GPHUD.Modules.External;
 
 import net.coagulate.GPHUD.Modules.Module.ModuleDefinition;
