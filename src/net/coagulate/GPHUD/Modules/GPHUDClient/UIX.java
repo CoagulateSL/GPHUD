154b4a48d2321f93e003db4d7adb527ba4d81603
Iain Price (LUNA) <git@predestined.net>
2020-05-02 16:46:57 +0100

    Remove test code, label 'call' properly

--- src/net/coagulate/GPHUD/Modules/GPHUDClient/UIX.java
+++ src/net/coagulate/GPHUD/Modules/GPHUDClient/UIX.java
@@ -1,7 +1,5 @@
 package net.coagulate.GPHUD.Modules.GPHUDClient;
 
-import net.coagulate.GPHUD.Data.Menu;
-import net.coagulate.GPHUD.Interfaces.Responses.JSONResponse;
 import net.coagulate.GPHUD.Interfaces.Responses.Response;
 import net.coagulate.GPHUD.Modules.Argument.ArgumentType;
 import net.coagulate.GPHUD.Modules.Argument.Arguments;
@@ -9,60 +7,19 @@ import net.coagulate.GPHUD.Modules.Command.Commands;
 import net.coagulate.GPHUD.Modules.Command.Context;
 import net.coagulate.GPHUD.Modules.Modules;
 import net.coagulate.GPHUD.State;
-import org.json.JSONObject;
 
 import javax.annotation.Nonnull;
 
 public class UIX {
 
 	// ---------- STATICS ----------
-	@Commands(description="Test only", permitUserWeb=false, permitScripting=false, permitObject=false, permitExternal=false, context=Context.ANY) @Nonnull
-	public static Response uixtest(@Nonnull final State state) {
-		JSONObject json=new JSONObject();
-
-		Menu menu=Menu.getMenu(state,"Main");
-		JSONObject m=menu.getJSON();
-		int elements=0;
-		for (int i=1;i<=12;i++) {
-			if (m.has("button"+i)) {
-				elements=i;
-				json.put("line"+i,m.getString("button"+i));
-				json.put("command"+i,m.getString("command"+i));
-			}
-		}
-
-		return new JSONResponse(json);
-	}
-
-	@Commands(description="Test only", permitUserWeb=false, permitScripting=false, permitObject=false, permitExternal=false, context=Context.ANY) @Nonnull
-	public static Response uixtest2(@Nonnull final State state) {
-		JSONObject json=new JSONObject();
-
-		Menu menu=Menu.getMenu(state,"Main");
-		JSONObject m=menu.getJSON();
-		int elements=0;
-		json.put("line1","Potion of Potato Peeling +2");
-		json.put("line2","Lesser Potato Peeler");
-		json.put("line3","A Potato Peeling");
-		json.put("line4","A Peeled Potato of Insight");
-		json.put("line5","A Long Potato Peel Strip");
-		json.put("line6","A Piece Of Potato");
-		json.put("line7","Mr. Potato Head");
-		json.put("line8","Potato Eye");
-		json.put("line9","Potato Potion of Starchiness +1");
-		json.put("line10","Royal Potato");
-		json.put("line11","Potato Ransom Note");
-		json.put("line12","Potato of Potato Potatoness");
-
-		return new JSONResponse(json);
-	}
-
 	@Nonnull
-	@Commands(description="Test only",
+	@Commands(description="UIX invoking method, HUD use only",
 	          permitUserWeb=false,
 	          permitScripting=false,
 	          permitObject=false,
 	          permitExternal=false,
+	          permitConsole=false,
 	          context=Context.ANY)
 	public static Response call(
 			@Nonnull
