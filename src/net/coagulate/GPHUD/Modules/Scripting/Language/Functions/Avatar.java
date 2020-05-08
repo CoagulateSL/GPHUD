fa533beb444a5ca2497df7f3eb56bfe20035c1d9
Iain Price (LUNA) <git@predestined.net>
2020-05-08 03:24:00 +0100

    ability to check avatar's permissions

---
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Avatar.java
@@ -0,0 +1,27 @@
+package net.coagulate.GPHUD.Modules.Scripting.Language.Functions;
+
+import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCInteger;
+import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCString;
+import net.coagulate.GPHUD.Modules.Scripting.Language.Functions.GSFunctions.GSFunction;
+import net.coagulate.GPHUD.Modules.Scripting.Language.GSVM;
+import net.coagulate.GPHUD.State;
+
+import javax.annotation.Nonnull;
+
+public class Avatar {
+
+	// ---------- STATICS ----------
+	@GSFunction(description="Checks if the controller of the current character has a particular permission",
+	            privileged=false,
+	            returns="Integer - 0 if they do not have the permission, 1 if they do",
+	            parameters="String - name of the permission to check",
+	            notes="")
+	public static BCInteger gsHasPermission(@Nonnull final State st,
+	                                        @Nonnull final GSVM vm,
+	                                        @Nonnull final BCString permission) {
+		String check=permission.getContent();
+		if (st.hasPermission(check)) { return new BCInteger(null,1); }
+		return new BCInteger(null,0);
+	}
+
+}
