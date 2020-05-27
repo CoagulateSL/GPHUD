84df980a30fc7230f0228a57106b492a1e5791f8
Iain Price (LUNA) <git@predestined.net>
2020-05-27 01:24:16 +0100

    log name of retired region that is incorrectly accessed

--- src/net/coagulate/GPHUD/Data/Region.java
+++ src/net/coagulate/GPHUD/Data/Region.java
@@ -91,7 +91,7 @@ public class Region extends TableRow {
 	                         final boolean allowretired) {
 		final Region r=(Region) factoryPut("Region",id,new Region(id));
 		if (r.isRetired() && (!allowretired)) {
-			final UserException exception=new UserInputStateException("Attempt to access retired region");
+			final UserException exception=new UserInputStateException("Attempt to access retired region "+r.getName());
 			GPHUD.getLogger("Regions").log(WARNING,"Attempt to access retired region",exception);
 			throw exception;
 		}
