96c8fe7051e1e9def1249036970d26df1e88a0a5
Iain Price (LUNA) <git@predestined.net>
2020-12-16 04:58:05 +0000

    Add functions gsGetPlayersName and gsGetPlayersUUID

--- src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Character.java
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Character.java
@@ -3,8 +3,10 @@ package net.coagulate.GPHUD.Modules.Scripting.Language.Functions;
 import net.coagulate.GPHUD.Modules.Characters.CharactersModule;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCCharacter;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCInteger;
+import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCString;
 import net.coagulate.GPHUD.Modules.Scripting.Language.GSVM;
 import net.coagulate.GPHUD.State;
+import net.coagulate.SL.Data.User;
 
 import javax.annotation.Nonnull;
 
@@ -21,6 +23,38 @@ public class Character {
                                                BCCharacter character) {
         State astate=new State(character.getContent());
         return new BCInteger(null,CharactersModule.abilityPointsRemaining(astate));
+    }
+
+    @Nonnull
+    @GSFunctions.GSFunction(description = "Gets the Avatar's name currently playing a character",
+                            category = GSFunctions.SCRIPT_CATEGORY.CHARACTER,
+                            notes = "Returns the empty string if not currently logged in",
+                            parameters="Character character - the character to query",
+                            privileged = false,
+                            returns = "String - Name of the avatar playing the character, or the empty string if not being played")
+    public static BCString gsGetPlayersName(@Nonnull final State state,
+                                            GSVM gsvm,
+                                            BCCharacter character) {
+        User currentPlayer=character.getContent().getPlayedByNullable();
+        String currentPlayerName="";
+        if (currentPlayer!=null) { currentPlayerName=currentPlayer.getName(); }
+        return new BCString(null,currentPlayerName);
+    }
+
+    @Nonnull
+    @GSFunctions.GSFunction(description = "Gets the Avatar's UUID currently playing a character",
+                            category = GSFunctions.SCRIPT_CATEGORY.CHARACTER,
+                            notes = "Returns the empty string if not currently logged in",
+                            parameters="Character character - the character to query",
+                            privileged = false,
+                            returns = "String - SL UUID of the avatar playing the character, or the empty string if not being played")
+    public static BCString gsGetPlayersUUID(@Nonnull final State state,
+                                            GSVM gsvm,
+                                            BCCharacter character) {
+        User currentPlayer=character.getContent().getPlayedByNullable();
+        String currentPlayerUUID="";
+        if (currentPlayer!=null) { currentPlayerUUID=currentPlayer.getUUID(); }
+        return new BCString(null,currentPlayerUUID);
     }
 
 }
