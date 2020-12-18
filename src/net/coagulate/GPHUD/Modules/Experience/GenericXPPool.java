0f577f573afd03f0af08b25d317db39281eb5eae
Iain Price (LUNA) <git@predestined.net>
2020-12-18 16:57:58 +0000

    Evaluate the Max Level of the target state not the caller's state

--- src/net/coagulate/GPHUD/Modules/Experience/GenericXPPool.java
+++ src/net/coagulate/GPHUD/Modules/Experience/GenericXPPool.java
@@ -62,9 +62,9 @@ public class GenericXPPool extends Pool {
 		}
 		if (reason==null) { throw new UserInputEmptyException("You must supply a reason"); }
 		// else award xp :P
-		int maxLevel=st.getKV("Experience.MaxLevel").intValue();
+		int maxLevel=targetstate.getKV("Experience.MaxLevel").intValue();
 		if (maxLevel==0) { maxLevel=1000; }
-		if (st.getCharacter().getLevel(st)>=maxLevel) { throw new UserInputStateException("This character is already at maximum level and can not be awarded any more experience of any type"); }
+		if (targetstate.getCharacter().getLevel(targetstate)>=maxLevel) { throw new UserInputStateException("This character is already at maximum level and can not be awarded any more experience of any type"); }
 		Audit.audit(st,Audit.OPERATOR.CHARACTER,null,target,"Pool Add",pool.name(),null,ammount+"",reason);
 		CharacterPool.addPool(st,target,pool,ammount,reason);
 		if (target!=st.getCharacterNullable()) {
