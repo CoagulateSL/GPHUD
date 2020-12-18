e517ea2fd7cd651f69c85257da59bd5ea39082af
Iain Price (LUNA) <git@predestined.net>
2020-12-18 06:58:28 +0000

    New module with a couple of basic set "admin" commands for now

---
+++ src/net/coagulate/GPHUD/Modules/Sets/Sets.java
@@ -0,0 +1,89 @@
+package net.coagulate.GPHUD.Modules.Sets;
+
+import net.coagulate.Core.Exceptions.System.SystemImplementationException;
+import net.coagulate.Core.Exceptions.User.UserAccessDeniedException;
+import net.coagulate.Core.Exceptions.User.UserInputStateException;
+import net.coagulate.GPHUD.Data.Attribute;
+import net.coagulate.GPHUD.Data.Audit;
+import net.coagulate.GPHUD.Data.Char;
+import net.coagulate.GPHUD.Data.CharacterSet;
+import net.coagulate.GPHUD.Interfaces.Responses.OKResponse;
+import net.coagulate.GPHUD.Interfaces.Responses.Response;
+import net.coagulate.GPHUD.Modules.Argument;
+import net.coagulate.GPHUD.Modules.Command;
+import net.coagulate.GPHUD.State;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class Sets {
+
+    private static void preCheck(State st,Attribute set,Char character) {
+        // Attribute must be a set
+        if (!(set.getType()== Attribute.ATTRIBUTETYPE.SET)) { throw new UserInputStateException("Attribute "+set.getName()+" is of type "+set.getType()+" not SET"); }
+        // Attribute must belong to instance (!)
+        if (!(set.getInstance()==st.getInstance())) { throw new SystemImplementationException("Attribute "+set+" is not from instance "+st.getInstanceString()); }
+        // check permission
+        if (!st.hasPermission("Characters.Set"+set.getName())) { throw new UserAccessDeniedException("You do not have permission Characters.Set"+set.getName()+" required to modify this characters set"); }
+        // check character is of right instance
+        if (!(st.getInstance()==character.getInstance())) { throw new SystemImplementationException("Target character "+character+" is not from instance "+st.getInstanceString()); }
+    }
+
+    @Nonnull
+    @Command.Commands(context=Command.Context.AVATAR,
+                      description="Add a number of an element to a set")
+    public static Response add(@Nonnull final State st,
+                               @Nonnull @Argument.Arguments(type = Argument.ArgumentType.CHARACTER,
+                                                            description="Character to modify",
+                                                            name="character") final Char character,
+                               @Nonnull @Argument.Arguments(type = Argument.ArgumentType.ATTRIBUTE,
+                                                            description = "Set to add to",
+                                                            name = "set") final Attribute set,
+                               @Nonnull @Argument.Arguments(description = "Element to add to set",
+                                                            type = Argument.ArgumentType.TEXT_ONELINE,
+                                                            name = "element",
+                                                            max = 128) final String element,
+                               @Nullable @Argument.Arguments(name = "qty",
+                                                             type = Argument.ArgumentType.INTEGER,
+                                                             description = "Number of element to add (or remove, if negative)",
+                                                             mandatory = false) Integer qty) {
+        preCheck(st,set,character);
+        if (qty==null) { qty=1; }
+        // guess we're ok then (!)
+        CharacterSet characterSet=new CharacterSet(character,set);
+        int oldValue=characterSet.count(element);
+        int total=characterSet.add(element,qty);
+        Audit.audit(true,st, Audit.OPERATOR.AVATAR,null,character,"Add",set.getName(),""+oldValue,""+total,"Added "+qty+" "+element+" to set, totalling "+total);
+        return new OKResponse("Added "+qty+" "+element+" to "+character+"'s "+set+", changing total from "+oldValue+" to "+total);
+    }
+
+    @Nonnull
+    @Command.Commands(context=Command.Context.AVATAR,
+                      description="Set the number of an element in a set")
+    public static Response set(@Nonnull final State st,
+                               @Nonnull @Argument.Arguments(type = Argument.ArgumentType.CHARACTER,
+                                                            description="Character to modify",
+                                                            name="character") final Char character,
+                               @Nonnull @Argument.Arguments(type = Argument.ArgumentType.ATTRIBUTE,
+                                                            description = "Set to add to",
+                                                            name = "set") final Attribute set,
+                               @Nonnull @Argument.Arguments(description = "Element to add to set",
+                                                            type = Argument.ArgumentType.TEXT_ONELINE,
+                                                            name = "element",
+                                                            max = 128) final String element,
+                               @Nullable @Argument.Arguments(name = "qty",
+                                                            type = Argument.ArgumentType.INTEGER,
+                                                            description = "Quantity to set to",
+                                                            mandatory = false) Integer qty) {
+        preCheck(st,set,character);
+        if (qty==null) { qty=1; }
+        // guess we're ok then (!)
+        CharacterSet characterSet=new CharacterSet(character,set);
+        int oldValue=characterSet.count(element);
+        characterSet.set(element,qty);
+        Audit.audit(true,st, Audit.OPERATOR.AVATAR,null,character,"Set",set.getName(),""+oldValue,""+qty,"Set "+qty+" x "+element);
+        return new OKResponse("Set "+qty+" "+element+" in "+character+"'s "+set+" (was "+oldValue+")");
+    }
+
+
+}
---
+++ src/net/coagulate/GPHUD/Modules/Sets/package-info.java
@@ -0,0 +1,7 @@
+@Module.ModuleDefinition(description="Provides commands for managing Character Sets",
+                         canDisable = false)
+
+package net.coagulate.GPHUD.Modules.Sets;
+
+import net.coagulate.GPHUD.Modules.Module;
+
