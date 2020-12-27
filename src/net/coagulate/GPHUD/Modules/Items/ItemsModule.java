ad26b462293c02c20bab092cbe2ff401c4437ff2
Iain Price (LUNA) <git@predestined.net>
2020-12-27 07:08:29 +0000

    Items module

---
+++ src/net/coagulate/GPHUD/Modules/Items/ItemsModule.java
@@ -0,0 +1,9 @@
+package net.coagulate.GPHUD.Modules.Items;
+
+import net.coagulate.GPHUD.Modules.ModuleAnnotation;
+
+public class ItemsModule extends ModuleAnnotation {
+    public ItemsModule(String name, ModuleDefinition def) {
+        super(name, def);
+    }
+}
---
+++ src/net/coagulate/GPHUD/Modules/Items/package-info.java
@@ -0,0 +1,8 @@
+@Module.ModuleDefinition(description = "Item Management",
+                         forceConfig = true,
+                         implementation = "net.coagulate.GPHUD.Modules.Items.ItemsModule",
+                         canDisable = false)
+
+package net.coagulate.GPHUD.Modules.Items;
+
+import net.coagulate.GPHUD.Modules.Module;
