8aff1cfa33a7871fe4ec49c3d45dca0786ec557c
Iain Price (LUNA) <git@predestined.net>
2021-01-16 11:47:38 +0000

    reformat

--- src/net/coagulate/GPHUD/Modules/Menus/MenuModule.java
+++ src/net/coagulate/GPHUD/Modules/Menus/MenuModule.java
@@ -19,87 +19,89 @@ import java.util.TreeMap;
  */
 public class MenuModule extends ModuleAnnotation {
 
-	public static final int MAXBUTTONS=40;
+    public static final int MAXBUTTONS = 40;
 
-	public MenuModule(final String name,
-	                  final ModuleDefinition annotation) {
-		super(name,annotation);
-	}
+    public MenuModule(final String name,
+                      final ModuleDefinition annotation) {
+        super(name, annotation);
+    }
 
-// ---------- STATICS ----------
-@Nonnull
-@Template(name="MAINMENU",
-          description="The packaged form of this users main menu")
-public static String generateMainMenu(final State st,
-                                      final String key) {
-	if (st.mainMenuTemplate == null) {
-		JSONObject menu=Modules.getJSONTemplate(st, "menus.main");
-		if (st.getCharacter().getProtocol()>=4) { // shortened format
-			int button=0;
-			StringBuilder newMainMenuTemplate=new StringBuilder();
+    // ---------- STATICS ----------
+    @Nonnull
+    @Template(name = "MAINMENU",
+              description = "The packaged form of this users main menu")
+    public static String generateMainMenu(final State st,
+                                          final String key) {
+        if (st.mainMenuTemplate == null) {
+            JSONObject menu = Modules.getJSONTemplate(st, "menus.main");
+            if (st.getCharacter().getProtocol() >= 4) { // shortened format
+                int button = 0;
+                StringBuilder newMainMenuTemplate = new StringBuilder();
 
-			while (menu.has("arg0button"+button)) {
-				if (!st.mainMenuTemplate.isBlank()) { newMainMenuTemplate.append("|"); }
-				newMainMenuTemplate.append(menu.getString("arg0button"+button));
-				button++;
-			}
-			st.mainMenuTemplate=newMainMenuTemplate.toString();
-		} else {
-			st.mainMenuTemplate = menu.toString();
-		}
-	}
-	return st.mainMenuTemplate;
-}
+                while (menu.has("arg0button" + button)) {
+                    if (!st.mainMenuTemplate.isBlank()) {
+                        newMainMenuTemplate.append("|");
+                    }
+                    newMainMenuTemplate.append(menu.getString("arg0button" + button));
+                    button++;
+                }
+                st.mainMenuTemplate = newMainMenuTemplate.toString();
+            } else {
+                st.mainMenuTemplate = menu.toString();
+            }
+        }
+        return st.mainMenuTemplate;
+    }
 
-	@Override
-	public boolean isGenerated() {
-		return false;
-	}
+    @Override
+    public boolean isGenerated() {
+        return false;
+    }
 
-	@Nonnull
-	@Override
-	public Command getCommandNullable(@Nonnull final State st,
-	                                  @Nonnull final String commandname) {
-		return new MenuCommand(st,commandname,Menu.getMenu(st,commandname).getJSON());
-	}
+    @Nonnull
+    @Override
+    public Command getCommandNullable(@Nonnull final State st,
+                                      @Nonnull final String commandname) {
+        return new MenuCommand(st, commandname, Menu.getMenu(st, commandname).getJSON());
+    }
 
-	@Nonnull
-	@Override
-	public Map<String,Command> getCommands(@Nonnull final State st) {
-		final Map<String,Command> commands=new TreeMap<>();
-		final Map<String,JSONObject> templates=Menu.getTemplates(st);
-		for (final Map.Entry<String,JSONObject> entry: templates.entrySet()) {
-			final String name=entry.getKey();
-			commands.put(name,new MenuCommand(st,name,entry.getValue()));
-		}
-		return commands;
-	}
+    @Nonnull
+    @Override
+    public Map<String, Command> getCommands(@Nonnull final State st) {
+        final Map<String, Command> commands = new TreeMap<>();
+        final Map<String, JSONObject> templates = Menu.getTemplates(st);
+        for (final Map.Entry<String, JSONObject> entry : templates.entrySet()) {
+            final String name = entry.getKey();
+            commands.put(name, new MenuCommand(st, name, entry.getValue()));
+        }
+        return commands;
+    }
 
-	@Override
-	protected void initialiseInstance(@Nonnull final State st) {
-		// some useful defaults
-		JSONObject j=new JSONObject();
-		j.put("button1","Roll");
-		j.put("command1","Roller.Roll");
-		j.put("button2","Roll Against");
-		j.put("command2","Roller.RollAgainst");
-		j.put("button3","Default Roll");
-		j.put("command3","Alias.Roll");
-		j.put("button4","Open Website");
-		j.put("command4","GPHUDClient.OpenWebsite");
-		j.put("button5","Damage Roll");
-		j.put("command5","Alias.Damage");
-		j.put("button6","Faction");
-		j.put("command6","Menus.Faction");
-		Menu.create(st,"Main","Main Menu",j);
-		j=new JSONObject();
-		j.put("button1","AwardXP");
-		j.put("command1","Faction.AwardXP");
-		j.put("button2","Invite");
-		j.put("command2","Faction.Invite");
-		Menu.create(st,"Faction","Faction Menu",j);
-	}
+    @Override
+    protected void initialiseInstance(@Nonnull final State st) {
+        // some useful defaults
+        JSONObject j = new JSONObject();
+        j.put("button1", "Roll");
+        j.put("command1", "Roller.Roll");
+        j.put("button2", "Roll Against");
+        j.put("command2", "Roller.RollAgainst");
+        j.put("button3", "Default Roll");
+        j.put("command3", "Alias.Roll");
+        j.put("button4", "Open Website");
+        j.put("command4", "GPHUDClient.OpenWebsite");
+        j.put("button5", "Damage Roll");
+        j.put("command5", "Alias.Damage");
+        j.put("button6", "Faction");
+        j.put("command6", "Menus.Faction");
+        Menu.create(st, "Main", "Main Menu", j);
+        j = new JSONObject();
+        j.put("button1", "AwardXP");
+        j.put("command1", "Faction.AwardXP");
+        j.put("button2", "Invite");
+        j.put("command2", "Faction.Invite");
+        Menu.create(st, "Faction", "Faction Menu", j);
+    }
 
-	// ---------- INSTANCE ----------
+    // ---------- INSTANCE ----------
 
 }
