ba8e41eee5fe71582baf6b0d28ca3a94b90c9b21
Iain Price (LUNA) <git@predestined.net>
2021-03-14 14:58:42 +0000

    Don't cache side sub menus (!!!) as they vary from user to user.  Also provide "canConfigure" method to check if the config page is accessible to this permissions.

--- src/net/coagulate/GPHUD/Modules/Configuration/ConfigurationModule.java
+++ src/net/coagulate/GPHUD/Modules/Configuration/ConfigurationModule.java
@@ -1,11 +1,10 @@
 package net.coagulate.GPHUD.Modules.Configuration;
 
 import net.coagulate.GPHUD.Modules.Module;
-import net.coagulate.GPHUD.Modules.ModuleAnnotation;
-import net.coagulate.GPHUD.Modules.Modules;
-import net.coagulate.GPHUD.Modules.SideSubMenu;
+import net.coagulate.GPHUD.Modules.*;
 import net.coagulate.GPHUD.State;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.Map;
@@ -19,39 +18,45 @@ import java.util.TreeMap;
  */
 public class ConfigurationModule extends ModuleAnnotation {
 
-	@Nullable
-	Set<SideSubMenu> submenus;
-
 	public ConfigurationModule(final String name,
 	                           final ModuleDefinition def) {
 		super(name,def);
-		submenus=null;
 	}
 
 	// ---------- INSTANCE ----------
 	@Nullable
 	@Override
 	public Set<SideSubMenu> getSideSubMenus(final State st) {
-		if (submenus==null) {
-			submenus=new HashSet<>();
-			final Map<String,SideSubMenu> map=new TreeMap<>();
-			for (final Module m: Modules.getModules()) {
-				if (m.isEnabled(st)) {
-					if (m.alwaysHasConfig() || !m.getKVDefinitions(st).isEmpty()) {
-						map.put(m.getName(),new ConfigurationSideSubMenu(m));
+		Set<SideSubMenu> submenus=new HashSet<>();
+		final Map<String,SideSubMenu> map=new TreeMap<>();
+		for (final Module m: Modules.getModules()) {
+			if (m.isEnabled(st)) {
+				if (m.alwaysHasConfig() || !m.getKVDefinitions(st).isEmpty()) {
+					map.put(m.getName(),new ConfigurationSideSubMenu(m));
+					if (canConfigure(st,m.getName())) {
 						submenus.add(map.get(m.getName()));
 					}
 				}
 			}
-			int priority=1;
-			for (final SideSubMenu sideSubMenu: map.values()) {
-				((ConfigurationSideSubMenu) sideSubMenu).setPriority(priority);
-				priority++;
-			}
 		}
-
+		int priority=1;
+		for (final SideSubMenu sideSubMenu: map.values()) {
+			((ConfigurationSideSubMenu) sideSubMenu).setPriority(priority);
+			priority++;
+		}
 		return submenus;
 	}
 
-
+	public static boolean canConfigure(@Nonnull final State state,
+									   @Nonnull String moduleName) {
+		URL urlHandler = Modules.getURL(state, "/configuration/" + moduleName);
+		if (urlHandler.requiresPermission().isBlank() || state.hasPermission(urlHandler.requiresPermission())) {
+			return true;
+		}
+		return false;
+	}
+	public static boolean canConfigure(@Nonnull final State state,
+									   @Nonnull Module module) {
+		return canConfigure(state,module.getName());
+	}
 }
