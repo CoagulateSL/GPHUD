41af089b2f095b3a94f50d04f99368859a40ba92
Iain Price (LUNA) <git@predestined.net>
2021-06-18 06:21:29 +0100

    New GSFunction to tell if a user is in a specific group by name.

--- src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Groups.java
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Groups.java
@@ -1,7 +1,9 @@
 package net.coagulate.GPHUD.Modules.Scripting.Language.Functions;
 
+import net.coagulate.Core.Exceptions.User.UserInputLookupFailureException;
 import net.coagulate.GPHUD.Data.CharacterGroup;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCCharacter;
+import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCInteger;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCList;
 import net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode.BCString;
 import net.coagulate.GPHUD.Modules.Scripting.Language.Functions.GSFunctions.SCRIPT_CATEGORY;
@@ -14,6 +16,23 @@ public class Groups {
 	private Groups() {}
 
 	// ---------- STATICS ----------
+	@Nonnull
+	@GSFunctions.GSFunction(description="Returns true(1) or false(0) depending on if the character is in the group",
+							returns="Integer - 0 if the character is not in the group, 1 if they are",
+							notes="",
+							parameters="Character - character to check<br>String - name of group to check for membership of",
+							privileged=false,
+							category= SCRIPT_CATEGORY.GROUPS)
+	public static BCInteger gsMemberOf(final State st,
+											final GSVM vm,
+											@Nonnull final BCCharacter target,
+											@Nonnull final BCString groupName) {
+		final CharacterGroup group=CharacterGroup.resolve(st,groupName.getContent());
+		if (group==null) { throw new UserInputLookupFailureException("Could not find group '"+groupName+"'",true); }
+		if (group.hasMember(target.getContent())) { return new BCInteger(null,1); }
+		return new BCInteger(null,0);
+	}
+
 	@Nonnull
 	@GSFunctions.GSFunction(description="Gets the group name for a given attribute",
 	                        returns="String - name of group of appropriate subtype, or the empty string if none",
