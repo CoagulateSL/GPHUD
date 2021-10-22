8e2cd0e32a1d650bbfedac759f241c08a7864ca3
Iain Price (LUNA) <git@predestined.net>
2021-10-22 13:56:56 +0100

    Local updates for javacc generation

--- src/net/coagulate/GPHUD/Modules/Scripting/Language/parser.bat
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/parser.bat
@@ -1,5 +1,5 @@
 c:
 del /s /f /q Generated
 cd \Users\iain\Documents\Git\SL\src\net\coagulate\GPHUD\Modules\Scripting\Language
-java -cp "c:\Program Files\javacc\bin\javacc.jar" jjtree GSParser.jjtree
-java -cp "c:\Program Files\javacc\bin\javacc.jar" javacc Generated/GSParser.jj
+"C:\Program Files\Java\jdk-11.0.2\bin\java" -cp "c:\Program Files\javacc\bin\javacc.jar" jjtree GSParser.jjtree
+"C:\Program Files\Java\jdk-11.0.2\bin\java" -cp "c:\Program Files\javacc\bin\javacc.jar" javacc Generated/GSParser.jj
