7bb53ec32360c8a5e43dd195fac54aa3d6ed0467
Iain Price (LUNA) <git@predestined.net>
2021-10-27 14:51:54 +0100

    suppressions

--- src/net/coagulate/GPHUD/Modules/Sets/Sets.java
+++ src/net/coagulate/GPHUD/Modules/Sets/Sets.java
@@ -23,11 +23,11 @@ public class Sets {
 
     private static void preCheck(State st,Attribute set,Char character,boolean checkAdmin) {
         // Attribute must be a set
-        if (!(set.getType()== Attribute.ATTRIBUTETYPE.SET)) { throw new UserInputStateException("Attribute "+set.getName()+" is of type "+set.getType()+" not SET"); }
+        if (!(set.getType()== Attribute.ATTRIBUTETYPE.SET)) { throw new UserInputStateException("Attribute "+set.getName()+" is of type "+set.getType()+" not SET",true); }
         // Attribute must belong to instance (!)
         if (!(set.getInstance()==st.getInstance())) { throw new SystemImplementationException("Attribute "+set+" is not from instance "+st.getInstanceString()); }
         // check permission
-        if (checkAdmin && !st.hasPermission("Characters.Set"+set.getName())) { throw new UserAccessDeniedException("You do not have permission Characters.Set"+set.getName()+" required to modify this characters set"); }
+        if (checkAdmin && !st.hasPermission("Characters.Set"+set.getName())) { throw new UserAccessDeniedException("You do not have permission Characters.Set"+set.getName()+" required to modify this characters set",true); }
         // check character is of right instance
         if (!(st.getInstance()==character.getInstance())) { throw new SystemImplementationException("Target character "+character+" is not from instance "+st.getInstanceString()); }
     }
