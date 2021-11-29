dcaf7ad990207d7be183e446174c39275cf42dba
Iain Price (LUNA) <git@predestined.net>
2021-11-29 00:54:23 +0000

    Use correct operation to add elements to the EXISTING list.

--- src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Sets.java
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/Functions/Sets.java
@@ -156,8 +156,8 @@ public class Sets {
         CharacterSet source=getSet(st,character,set);
         BCList list=new BCList(null);
         for(Map.Entry<String,Integer> element:source.elements().entrySet()) {
-            list.add(new BCString(null,element.getKey()));
-            list.add(new BCInteger(null,element.getValue()));
+            list.append(new BCString(null,element.getKey()));
+            list.append(new BCInteger(null,element.getValue()));
         }
         return list;
     }
