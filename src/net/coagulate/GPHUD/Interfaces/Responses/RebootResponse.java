b2cedd62f9db812110f60f6dc85bba0cababaff6
Iain Price (LUNA) <git@predestined.net>
2022-03-18 15:17:03 +0000

    Template for the reboot response

---
+++ src/net/coagulate/GPHUD/Interfaces/Responses/RebootResponse.java
@@ -0,0 +1,58 @@
+package net.coagulate.GPHUD.Interfaces.Responses;
+
+import net.coagulate.Core.Exceptions.System.SystemConsistencyException;
+import net.coagulate.GPHUD.Interfaces.Outputs.Renderable;
+import net.coagulate.GPHUD.State;
+import org.json.JSONObject;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
+
+/**
+ * A response that terminates the remote endpoint
+ *
+ * @author Iain Price <gphud@predestined.net>
+ */
+public class RebootResponse implements Response {
+
+	final String reason;
+
+	public RebootResponse(final String r) {
+		reason=r;
+	}
+
+	// ---------- INSTANCE ----------
+	@Nonnull
+	@Override
+	public JSONObject asJSON(final State st) {
+		final JSONObject j=new JSONObject();
+		j.put("reboot",reason);
+		return j;
+	}
+
+	@Nonnull
+	@Override
+	public String scriptResponse() {
+		return "This message contains code to reboot the receiver, however as you are a script, have fun with that.";
+	}
+
+	@Nonnull
+	@Override
+	public String asText(final State st) {
+		throw new SystemConsistencyException("This request is a reboot request - "+reason);
+	}
+
+	@Nonnull
+	@Override
+	public String asHtml(final State st,
+	                     final boolean rich) {
+		throw new SystemConsistencyException("This request is a reboot request - "+reason);
+	}
+
+	@Nullable
+	@Override
+	public Set<Renderable> getSubRenderables() {
+		return null;
+	}
+}
