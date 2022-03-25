5caea16aed5560fdbae073c7ac89f3cd7ccb936d
Iain Price (LUNA) <git@predestined.net>
2022-03-25 12:49:40 +0000

    Define "Short" caching interval as 5 seconds. I.e. used where a page computation may be longer than 1 second so we can reuse this computation (see most configuration pages with drop down repetition). Notably these caches are not flushed by changes to the data store at the moment so should be kept minimal to optimise that single page (future thought, page level cache structures are nice :P).

--- src/net/coagulate/GPHUD/Data/CacheConfig.java
+++ src/net/coagulate/GPHUD/Data/CacheConfig.java
@@ -2,6 +2,8 @@ package net.coagulate.GPHUD.Data;
 
 public final class CacheConfig {
 
+    private static final int ONE_SECOND=1;
+    private static final int FIVE_SECONDS=5;
     private static final int ONE_MINUTE=60;
     private static final int FIVE_MINUTES=ONE_MINUTE*5;
     private static final int FIFTEEN_MINUTES=ONE_MINUTE*15;
@@ -11,6 +13,7 @@ public final class CacheConfig {
     public static final int PERMANENT_CONFIG = ONE_HOUR; // cachable forever, but frees up memory.
     // Not all actually permanent but does imply that caching works properly at least.
     public static final int MUTABLE = ONE_MINUTE;
-    public static final int MINIMAL = 1; // for things that update, but we might want to cache for the remainder of this request
+    public static final int SHORT = FIVE_SECONDS;
+    public static final int MINIMAL = ONE_SECOND; // for things that update, but we might want to cache for the remainder of this request
 
 }
