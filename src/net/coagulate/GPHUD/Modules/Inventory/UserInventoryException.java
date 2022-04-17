f7df61588acd2f66988f5bd71a23775fd21d0046
Iain Price (LUNA) <git@predestined.net>
2022-04-17 23:02:36 +0100

    Add serial version to serialisable classes.  Then don't use it properly.

--- src/net/coagulate/GPHUD/Modules/Inventory/UserInventoryException.java
+++ src/net/coagulate/GPHUD/Modules/Inventory/UserInventoryException.java
@@ -3,6 +3,8 @@ package net.coagulate.GPHUD.Modules.Inventory;
 import net.coagulate.Core.Exceptions.User.UserInputStateException;
 
 public class UserInventoryException extends UserInputStateException {
+
+    private static final long serialVersionUID = 1L;
     public UserInventoryException(String reason) {
         super(reason);
     }
