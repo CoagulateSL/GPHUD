9b125cfc5cf61d5aa17b5f633a1a14c27950d3b6
Iain Price (LUNA) <git@predestined.net>
2022-04-19 12:05:33 +0100

    Cleaner

---
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/ByteCode/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all the operations supported by the GSVM, along with their byte code op values.
+ *
+ * @author Iain Price
+ */
+
+package net.coagulate.GPHUD.Modules.Scripting.Language.ByteCode;
