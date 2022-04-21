fa7c1637d422765331b3c3e87031abd2ec1f9439
Iain Price (LUNA) <git@predestined.net>
2022-04-21 13:57:23 +0100

    Clean up all inspection points on edited file.

---
+++ src/net/coagulate/GPHUD/Data/package-info.java
@@ -0,0 +1,6 @@
+/** This package handles database and SQL abstraction for GPHUD.
+ * <p>
+ * GPHUD supports "SQL calling path verification" - SQL calls are *ONLY* permitted from inside the Data package of GPHUD.
+ */
+
+package net.coagulate.GPHUD.Data;
