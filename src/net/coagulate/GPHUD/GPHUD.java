f92464cc4d093fa6aeb525cbe1ea7bdae306d1ca
Iain Price (LUNA) <git@predestined.net>
2022-04-23 22:11:08 +0100

    Reformat inspection

--- src/net/coagulate/GPHUD/GPHUD.java
+++ src/net/coagulate/GPHUD/GPHUD.java
@@ -157,7 +157,7 @@ public class GPHUD extends SLModule {
 	 */
 	@Nonnull
 	public static String serverVersion() {
-		return "[https://"+ Config.getURLHost()+"/GPHUD/ChangeLog "+SL.getModule("GPHUD").getBuildDateString()+" @"+SL.getModule("GPHUD").commitId()+"] (C) secondlife:///app/agent/"+Config.getCreatorUUID()+"/about";
+		return "[https://"+Config.getURLHost()+"/GPHUD/ChangeLog "+SL.getModule("GPHUD").getBuildDateString()+" @"+SL.getModule("GPHUD").commitId()+"] (C) secondlife:///app/agent/"+Config.getCreatorUUID()+"/about";
 	}
 	
 	/**
