ad54567a7601b190f7da1e6f207fb55e89a6bd28
Iain Price (LUNA) <git@predestined.net>
2022-04-23 23:25:32 +0100

    Reformat introspection

--- src/net/coagulate/GPHUD/Data/package-info.java
+++ src/net/coagulate/GPHUD/Data/package-info.java
@@ -5,7 +5,7 @@
  */
 
 @Change(type=CHANGETYPE.Fix, date="2022-04-22", component=COMPONENT.Core, message="Status updater no longer exceptions failing to find the running version at instances with no active regions (harmless silent error)")
-@Change(type=CHANGETYPE.Change,date="2022-04-23",component=COMPONENT.Core,message="Preserve the leveltext conveyance through the normal logon wipe, preventing the HUD from always reporting XP/level, now only reporting it at login if it has changed.")
+@Change(type=CHANGETYPE.Change, date="2022-04-23", component=COMPONENT.Core, message="Preserve the leveltext conveyance through the normal logon wipe, preventing the HUD from always reporting XP/level, now only reporting it at login if it has changed.")
 package net.coagulate.GPHUD.Data;
 
 import net.coagulate.GPHUD.Classes.COMPONENT;
