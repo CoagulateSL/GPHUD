6557e4fff2ed9493b6aab40fcfb4d34b16a5ee6f
Iain Price (LUNA) <git@predestined.net>
2022-04-24 03:21:21 +0100

    Reformat introspection

--- src/net/coagulate/GPHUD/Modules/Objects/package-info.java
+++ src/net/coagulate/GPHUD/Modules/Objects/package-info.java
@@ -1,35 +1,35 @@
 @Module.ModuleDefinition(description="Allows objects to connect in and be assigned behaviour",
-                         defaultDisable=true)
+		defaultDisable=true)
 
 @Permission.Permissions(name="Connect",
-                        description="Allowed to connect objects to GPHUD",
-                        power=Permission.POWER.MEDIUM)
+		description="Allowed to connect objects to GPHUD",
+		power=Permission.POWER.MEDIUM)
 
 @Permission.Permissions(name="View",
-                        description="Ability to view all connected objects",
-                        power=Permission.POWER.LOW)
+		description="Ability to view all connected objects",
+		power=Permission.POWER.LOW)
 
 @Permission.Permissions(name="ObjectTypes",
-                        description="Ability to create edit or delete object types (behaviours)",
-                        power=Permission.POWER.MEDIUM)
+		description="Ability to create edit or delete object types (behaviours)",
+		power=Permission.POWER.MEDIUM)
 
 @Permission.Permissions(name="MapObjects",
-                        description="The ability to map obejcts to object types",
-                        power=Permission.POWER.LOW)
+		description="The ability to map obejcts to object types",
+		power=Permission.POWER.LOW)
 
 @Permission.Permissions(name="RebootObjects",
-                        description="The ability to reboot in-world scripted objects",
-                        power=Permission.POWER.LOW)
+		description="The ability to reboot in-world scripted objects",
+		power=Permission.POWER.LOW)
 
 @Permission.Permissions(name="ShutdownObjects",
-                        description="The ability to shutdown in-world scripted objects",
-                        power=Permission.POWER.MEDIUM)
+		description="The ability to shutdown in-world scripted objects",
+		power=Permission.POWER.MEDIUM)
 
 @Permission.Permissions(name="GetDriver",
-                        description="Allowed to get a copy of the Object Driver script",
-                        power=Permission.POWER.MEDIUM)
+		description="Allowed to get a copy of the Object Driver script",
+		power=Permission.POWER.MEDIUM)
 
-@Change(date="2022-04-24",type=CHANGETYPE.Fix,component=COMPONENT.ObjectDriver,message="Phantom/Volume Detect/Collision object driver only detects avatar collisions.")
+@Change(date="2022-04-24", type=CHANGETYPE.Fix, component=COMPONENT.ObjectDriver, message="Phantom/Volume Detect/Collision object driver only detects avatar collisions.")
 
 package net.coagulate.GPHUD.Modules.Objects;
 
