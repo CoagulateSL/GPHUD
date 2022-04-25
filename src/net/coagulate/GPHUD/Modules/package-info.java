01d3d2c7850f98f3e242844a38a3c63e8ccc9884
Iain Price (LUNA) <git@predestined.net>
2022-04-25 03:36:18 +0100

    Change log

--- src/net/coagulate/GPHUD/Modules/package-info.java
+++ src/net/coagulate/GPHUD/Modules/package-info.java
@@ -1,3 +1,8 @@
 /** Defines the basic modules framework and datastructures used by modules */
 
+@Change(date="2022-04-25", type=CHANGETYPE.Add, component=COMPONENT.Core, message="Support for JSON enumerating permissions groups, allowing commands like PermissionsGroups.Invite to be used on a HUD menu")
 package net.coagulate.GPHUD.Modules;
+
+import net.coagulate.GPHUD.Classes.COMPONENT;
+import net.coagulate.GPHUD.Classes.Change;
+import net.coagulate.SL.ChangeLogging.CHANGETYPE;
