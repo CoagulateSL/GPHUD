3cd0282944ab09e1c627585580034b030903a192
Iain Price (LUNA) <git@predestined.net>
2023-03-23 00:07:41 +0000

    Revert "More oops"
    This reverts commit 256313c8ac04b976246d7985ee6810a3598e7aa2.

--- src/net/coagulate/GPHUD/Data/TableRow.java
+++ src/net/coagulate/GPHUD/Data/TableRow.java
@@ -128,7 +128,7 @@ public abstract class TableRow extends net.coagulate.Core.Database.TableRow impl
 	}
 
 	protected void setName(final String name) {
-		this.name=name;
+		TableRow.name=name;
 		set(getNameField(),name);
 	}
 	/**
