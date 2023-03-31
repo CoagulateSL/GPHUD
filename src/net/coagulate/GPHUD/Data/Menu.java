a559f48c437e2e0cf9cc3a2e23fa1f563471d3d1
Iain Price (LUNA) <git@predestined.net>
2023-03-31 01:42:30 +0100

    Cache menu name resolution

--- src/net/coagulate/GPHUD/Data/Menu.java
+++ src/net/coagulate/GPHUD/Data/Menu.java
@@ -9,6 +9,7 @@ import net.coagulate.Core.Exceptions.User.UserInputInvalidChoiceException;
 import net.coagulate.Core.Exceptions.User.UserInputLookupFailureException;
 import net.coagulate.Core.Exceptions.User.UserInputValidationParseException;
 import net.coagulate.Core.Exceptions.UserException;
+import net.coagulate.Core.Tools.Cache;
 import net.coagulate.GPHUD.Data.Audit.OPERATOR;
 import net.coagulate.GPHUD.State;
 import org.json.JSONObject;
@@ -75,24 +76,7 @@ public class Menu extends TableRow {
 		return ret;
 	}
 	
-	/**
-	 * Load instance menu by name
-	 *
-	 * @param st   State (infers instance)
-	 * @param name Name of the menu to load
-	 * @return Menus object
-	 */
-	@Nullable
-	public static Menu getMenuNullable(@Nonnull final State st,@Nonnull final String name) {
-		try {
-			final int id=db().dqiNotNull("select menuid from menus where instanceid=? and name like ?",
-			                             st.getInstance().getId(),
-			                             name);
-			return get(id);
-		} catch (@Nonnull final NoDataException e) {
-			return null;
-		}
-	}
+	private static final Cache<Menu,JSONObject> menuJsonCache=Cache.getCache("GPHUD/MenuJson",CacheConfig.PERMANENT_CONFIG);
 	
 	/**
 	 * Create a new menu,by name and description, with the given json data blob
@@ -128,6 +112,7 @@ public class Menu extends TableRow {
 					"Failed to create alias "+name+" in instance id "+st.getInstance().getId()+
 					", created but not found?");
 		}
+		st.getInstance().menuNameResolveCache.set(name,newalias);
 		return newalias;
 	}
 	
@@ -193,6 +178,31 @@ public class Menu extends TableRow {
 	}
 	
 	/**
+	 * Load instance menu by name
+	 *
+	 * @param st   State (infers instance)
+	 * @param name Name of the menu to load
+	 * @return Menus object
+	 */
+	@Nullable
+	public static Menu getMenuNullable(@Nonnull final State st,@Nonnull final String name) {
+		return st.getInstance().menuNameResolveCache.get(name,()->{
+			try {
+				final int id=db().dqiNotNull("select menuid from menus where instanceid=? and name like ?",
+				                             st.getInstance().getId(),
+				                             name);
+				return get(id);
+			} catch (@Nonnull final NoDataException e) {
+				return null;
+			}
+		});
+	}
+	
+	protected int getNameCacheTime() {
+		return CacheConfig.PERMANENT_CONFIG;
+	} // this name doesn't change, cache 1 hour
+	
+	/**
 	 * Deletes this menu, if its not the Main menu
 	 *
 	 * @param st The State
@@ -204,12 +214,9 @@ public class Menu extends TableRow {
 					"You can not delete the Main menu as this is hard wired to the main HUD button");
 		}
 		Audit.audit(true,st,OPERATOR.AVATAR,null,null,"delete","Menus",oldname,"","Deleted menu "+oldname);
+		st.getInstance().menuNameResolveCache.purge(oldname);
 		d("delete from menus where menuid=?",getId());
 	}
-	
-	protected int getNameCacheTime() {
-		return CacheConfig.PERMANENT_CONFIG;
-	} // this name doesn't change, cache 1 hour
 	
 	/**
 	 * Load the JSON payload for this menu.
@@ -218,11 +225,13 @@ public class Menu extends TableRow {
 	 */
 	@Nonnull
 	public JSONObject getJSON() {
-		final String json=dqs("select json from menus where menuid=?",getId());
-		if (json==null) {
-			throw new SystemBadValueException("No (null) template for menu id "+getId());
-		}
-		return new JSONObject(json);
+		return menuJsonCache.get(this,()->{
+			final String json=dqs("select json from menus where menuid=?",getId());
+			if (json==null) {
+				throw new SystemBadValueException("No (null) template for menu id "+getId());
+			}
+			return new JSONObject(json);
+		});
 	}
 	
 	/**
@@ -232,6 +241,7 @@ public class Menu extends TableRow {
 	 */
 	public void setJSON(@Nonnull final JSONObject template) {
 		d("update menus set json=? where menuid=?",template.toString(),getId());
+		menuJsonCache.set(this,template);
 	}
 	
 	/**
