afa415788aae48ea75abaf234163d5b9fd70eb5f
iain <git@predestined.net>
2024-10-27 15:33:55 +0000

    Separate out the current implementation to its new name of StackVM, this is a breaking change

---
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/GSJavaCompiler.java
@@ -0,0 +1,6 @@
+package net.coagulate.GPHUD.Modules.Scripting.Language;
+
+/** Compile GS code to java bytecode, hopefully safely :P */
+
+public class GSJavaCompiler extends GSCompiler {
+}
---
+++ src/net/coagulate/GPHUD/Modules/Scripting/Language/GSJavaVM.java
@@ -0,0 +1,6 @@
+package net.coagulate.GPHUD.Modules.Scripting.Language;
+
+/** A wrapper for the execution of scripts compiled as java code */
+
+public class GSJavaVM extends GSVM {
+}
