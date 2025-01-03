cef841ca6c8cc4cd83f7de29ace93c5596ff888a
iain <git@predestined.net>
2025-01-03 13:56:36 +0000

    Added Import code

--- src/net/coagulate/GPHUD/Modules/Transport/Transports/CharacterGroupTransport.java
+++ src/net/coagulate/GPHUD/Modules/Transport/Transports/CharacterGroupTransport.java
@@ -1,5 +1,6 @@
 package net.coagulate.GPHUD.Modules.Transport.Transports;
 
+import net.coagulate.GPHUD.Data.Audit;
 import net.coagulate.GPHUD.Data.CharacterGroup;
 import net.coagulate.GPHUD.Data.TableRow;
 import net.coagulate.GPHUD.Modules.Transport.ImportReport;
@@ -39,6 +40,53 @@ public class CharacterGroupTransport extends Transporter {
 	                             @Nonnull final String name,
 	                             @Nonnull final JSONObject element,
 	                             final boolean simulation) {
+		final boolean open=element.getBoolean("open");
+		final String subtype=element.getString("type");
+		final int kvprecedence=element.getInt("kvprecedence");
+		existCheck(state,
+		           simulation,
+		           report,
+		           CharacterGroup.resolve(state,name),
+		           name,
+		           ()->CharacterGroup.create(state.getInstance(),name,open,subtype));
+		final CharacterGroup cg=CharacterGroup.resolve(state,name);
+		kvRestore(state,simulation,report,element.getJSONObject("kvstore"),name,cg);
+		if (open!=cg.isOpen()) {
+			report.info("CharacterGroup - changing open flag on "+name+" to "+open);
+			if (!simulation) {
+				final boolean oldopen=cg.isOpen();
+				cg.setOpen(open);
+				Audit.audit(state,
+				            Audit.OPERATOR.AVATAR,
+				            null,
+				            null,
+				            "CharacterGroup Import",
+				            "Group Open",
+				            Boolean.toString(oldopen),
+				            Boolean.toString(open),
+				            "Changed open status via character group import");
+			}
+		}
+		if (kvprecedence!=cg.getKVPrecedence()) {
+			report.info("CharacterGroup - changing kvprecedence on "+name+" to "+kvprecedence);
+			if (!simulation) {
+				final int oldprecedence=cg.getKVPrecedence();
+				cg.setKVPrecedence(kvprecedence);
+				Audit.audit(state,
+				            Audit.OPERATOR.AVATAR,
+				            null,
+				            null,
+				            "CharacterGroup Import",
+				            "Group KV Precedence",
+				            Integer.toString(oldprecedence),
+				            Integer.toString(kvprecedence),
+				            "Changed kv precedence via character group import");
+			}
+		}
+		if (cg.getTypeNotNull().equals(subtype)) {
+			report.error("CharacterGroup - group sub type wants to change from '"+cg.getTypeNotNull()+"' to '"+subtype+
+			             "' but changing subtypes is not allowed");
+		}
 	}
 }
 
