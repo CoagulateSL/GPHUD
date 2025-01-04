4227b0e947b95494ce6c7f51c29fee207116f8d4
iain <git@predestined.net>
2025-01-04 19:37:53 +0000

    Change log

--- src/net/coagulate/GPHUD/Modules/Transport/package-info.java
+++ src/net/coagulate/GPHUD/Modules/Transport/package-info.java
@@ -10,7 +10,13 @@
 
 @SideMenus(name="Transport", priority=875, requiresPermission="transport.*", url="/transport")
 
+@Classes.Change(component=Classes.COMPONENT.Core,
+                type=ChangeLogging.CHANGETYPE.Add,
+                date="2025-01-04",
+                message="Added Transport module - used for importing and exporting configuration data")
 package net.coagulate.GPHUD.Modules.Transport;
 
+import net.coagulate.GPHUD.Classes;
 import net.coagulate.GPHUD.Modules.Module;
 import net.coagulate.GPHUD.Modules.SideMenu.SideMenus;
+import net.coagulate.SL.ChangeLogging;
