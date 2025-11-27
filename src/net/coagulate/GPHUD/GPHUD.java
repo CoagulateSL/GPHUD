76c4b7d9e55dea86535027d86b5ed21afc198d74
Iain <git@predestined.net>
2025-11-27 03:35:31 +0000

    Database schema for the new reporting

--- src/net/coagulate/GPHUD/GPHUD.java
+++ src/net/coagulate/GPHUD/GPHUD.java
@@ -219,7 +219,7 @@ public class GPHUD extends SLModule {
 		return GPHUDBuildInfo.BUILDDATE;
 	}
 	
-	private static final int SCHEMA_VERSION=19;
+	private static final int SCHEMA_VERSION=20;
 
 	@SuppressWarnings("MagicNumber")
 	@Override
@@ -409,6 +409,26 @@ public class GPHUD extends SLModule {
 			GPHUD.getDB().d("drop index zonekvstore_key on zonekvstore");
 			log.config("Schema upgrade of GPHUD to version 19 is complete");
 			currentVersion=19;
+		}
+		if (currentVersion==19) {
+			log.config("Schema update 20 - reporting rewrite");
+			GPHUD.getDB().d("ALTER TABLE `instances` DROP COLUMN `reporttds` DROP COLUMN `report`, "+
+			                "ADD COLUMN `reportstart` INT(11) NULL DEFAULT 0 AFTER `reporting`,"+
+			                "ADD COLUMN `reportend` INT(11) NULL DEFAULT 0 AFTER `reportstart`,"+
+			                "ADD COLUMN `instancescol` VARCHAR(45) NULL AFTER `retirewarn`,"+
+			                "CHANGE COLUMN `reporting` `reporting` TINYINT NULL DEFAULT 0");
+			GPHUD.getDB().d("CREATE TABLE `reports` (`instanceid` INT NOT NULL,"+
+			                "  `characterid` INT NOT NULL, `column` VARCHAR(128) NOT NULL,"+
+			                "  `data` VARCHAR(4096) NULL, PRIMARY KEY (`instanceid`, `characterid`, `column`),"+
+			                "  INDEX `reports_characterid_idx` (`characterid` ASC) VISIBLE,"+
+			                "  CONSTRAINT `reports_instanceid` FOREIGN KEY (`instanceid`)"+
+			                "    REFERENCES `gphuddev`.`instances` (`instanceid`) ON DELETE CASCADE"+
+			                "    ON UPDATE RESTRICT, CONSTRAINT `reports_characterid`"+
+			                "    FOREIGN KEY (`characterid`)"+
+			                "    REFERENCES `gphuddev`.`characters` (`characterid`) ON DELETE CASCADE"+
+			                "    ON UPDATE RESTRICT)");
+			log.config("Schema upgrade of GPHUD to version 20 is complete");
+			currentVersion=20;
 		}
 		return currentVersion;
 		// UPDATE THE CURRENT VERSION NUMBER ABOVE THIS FUNC
